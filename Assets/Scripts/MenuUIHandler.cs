1c68c463c9b0deeddc1f6c67b3cdc4acf972ad43
Keke Brown <fbrowndev@gmail.com>
2022-10-28 14:49:47 -0400

    Main Menu Button
    Set up the functionality for main menu start and stop

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -2,23 +2,49 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 // Sets the script to be executed later than all default scripts
 // This is helpful for UI, since other things may need to be initialized before setting the UI
 [DefaultExecutionOrder(1000)]
 public class MenuUIHandler : MonoBehaviour
 {
+    #region Menu Variables
     public ColorPicker ColorPicker;
+
+    #endregion
 
     public void NewColorSelected(Color color)
     {
         // add code here to handle when a color is selected
     }
     
-    private void Start()
+    void Start()
     {
         ColorPicker.Init();
         //this will call the NewColorSelected function when the color picker have a color button clicked.
         ColorPicker.onColorChanged += NewColorSelected;
     }
+
+
+    #region Start Exit Methods
+
+    public void NewStart()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+    Application.Quit();
+#endif
+    }
+
+#endregion
 }
